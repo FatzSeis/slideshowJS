f1556ea03e2a9a5b49fab7b2d97d842b02ee1e72
Fabrizio Terzi <131731777+FatzSeis@users.noreply.github.com>
2023-06-13 04:09:16 -0300

    Add files via upload

---
+++ slideshowprojetinho/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const images = [
+    { 'id': '1', 'url':'img/luffy.jpg' },
+    { 'id': '2', 'url':'img/zoro.jpg' },
+    { 'id': '3', 'url':'img/sanji.jpg' },
+    { 'id': '4', 'url':'img/robin.jpg' },
+    { 'id': '5', 'url':'img/usopp.jpg' },
+    { 'id': '6', 'url':'img/chopper.jpg' },
+]
+
+const containerItems = document.querySelector('#container-items');
+const containerIndicators = document.querySelector('.indicators');
+
+const createIndicators = (images, container) => {
+    images.forEach ( image => {
+        container.innerHTML += `<span data-number=${image.id}>${image.id}</span>`
+    })
+}
+
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
+        container.innerHTML += `
+            <div class='item' data-number=${image.id}>
+                <img src='${image.url}'
+            </div>
+        `
+    })
+}
+
+loadImages( images, containerItems );
+createIndicators(images, containerIndicators);
+
+let items = document.querySelectorAll('.item');
+
+const removeClassSelected = () => {
+    const indicators = document.querySelectorAll('span');
+    indicators.forEach( indicator => indicator.classList.remove ('selected'));
+} 
+
+const selectIndicator = (number) => {
+    removeClassSelected();
+    const indicator = document.querySelector(`span[data-number="${number}"]`)
+    indicator.classList.add('selected')
+} 
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+    selectIndicator (items[1].dataset.number)
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore( lastItem, items[0] );
+    items = document.querySelectorAll('.item');
+    selectIndicator (items[1].dataset.number)
+}
+
+
+
+
+const clickIndicators = ({target}) => {
+
+    if ( target.tagName == 'SPAN') {
+        const selectedIndicator = target.dataset.number;
+        let visibleSlide = items[1].dataset.number
+        if (selectedIndicator !== visibleSlide){
+            
+            const autoNext = setInterval ( () => {
+                document.querySelector('#next').click();
+                visibleSlide = items[1].dataset.number;
+                if (selectedIndicator == visibleSlide) clearInterval(autoNext);
+            }, 100);
+        }
+    }
+
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
+document.getElementById('indicators').addEventListener('click',clickIndicators);
